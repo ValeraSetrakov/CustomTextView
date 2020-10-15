2cd940b64b0cd28d1a93a9b1b8464ebf356e07f7
valerijsetrakov <valerasetrakov@gmail.com>
2020-10-15 23:27:55 +0300

    Enabled set text method for RendererTextView

--- app/src/main/java/com/valerasetrakov/customtextview/RendererTextView.kt
+++ app/src/main/java/com/valerasetrakov/customtextview/RendererTextView.kt
@@ -89,10 +89,6 @@ open class RendererTextView @JvmOverloads constructor(
         setText(text, BufferType.SPANNABLE)
     }
 
-    override fun setText(text: CharSequence?, type: BufferType?) {
-        error(UnsupportedOperationException("Use setRendererText method"))
-    }
-
     data class BackgroundAnnotationMeasure (
         val startLine: Int,
         val endLine: Int,
